ea07caad844e5843c2cffef9b78d44c3c724de16
Logan <10239411+Loag@users.noreply.github.com>
2021-11-11 10:25:20 -0500

    added comments to smallest common element in all rows

--- power_of_four.py
+++ power_of_four.py
@@ -1,5 +1,9 @@
 # 342
 
+'''
+  This is kind of a cheat but does provide what the question asks. 
+  Get the powers of 4 before hand and check if input in set.
+'''
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n in set(list([1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824]))
--- smallest_common_element_in_all_rows.py
+++ smallest_common_element_in_all_rows.py
@@ -5,12 +5,26 @@ from types import List
 
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
+        # new default dict with set as default
         cont = defaultdict(set)
+        # iterate over rows w/ index
         for index, row in enumerate(mat):
+            # iterate over values in the row
             for val in row:
+                # for the value, add the row index
                 cont[val].add(index)
         
+        # iterate over dict keys in order
         for k in sorted(list(cont.keys())):
+            '''
+              because the values are unique, if the 
+              length of the set of values is the same
+              as the length of rows, we know that the 
+              value is in each row. Because ordered
+              we will find the smallest first.
+            '''
             if len(cont[k]) == len(mat):
                 return k
+        
+        # if not default to return -1
         return -1
