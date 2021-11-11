ca550c7dfc0ad02ea62f82f8a41598bea6cd6b6d
Logan <10239411+Loag@users.noreply.github.com>
2021-11-11 10:31:54 -0500

    added comments to two sum bst

--- two_sum_bst.py
+++ two_sum_bst.py
@@ -10,6 +10,7 @@ class TreeNode:
 
 class Solution:
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:        
+        # traverse root 2 and gat all of its values.
         def rec2(node, s):
             if node:
                 s.add(node.val)
@@ -19,11 +20,18 @@ class Solution:
         
         t2 = rec2(root2, set())
         
+        # traverse root1
         def rec1(node):
             if node:
+
+                # get val from left tree
                 t = rec1(node.left)
+
+                # if target minus the value is in the second tree, then it is true
                 if target - node.val in t2:
                     return True
+
+                # if its not in the left tree, try the right
                 if not t:
                     t = rec1(node.right)
                 return t
