29db8f6bad05b0ef2e2aee099331e4ba40ee6568
Logan <10239411+Loag@users.noreply.github.com>
2021-11-11 10:21:17 -0500

    commented kill process and switched to defaultdict

--- kill_process.py
+++ kill_process.py
@@ -1,22 +1,34 @@
 # 582
 from types import List
+from collections import defaultdict
 
 class Solution:
     def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
-        cont = {}
+        # create new default dict with new list as the default
+        cont = defaultdict(list)
+        
+        # iterate over process, and process parent. 
         for p, pp in zip(pid, ppid):
-            if pp in cont:
-                cont[pp].append(p)
-            else:
-                cont[pp] = [p]
-                
+            # add the process to the parents list of children
+            cont[pp].append(p)
+        
+        # the nodes we are going to kill. Start with the node to kill.
         res = [kill]
+
+        # stack of nodes to iterate through
         st = [kill]
 
+        # while the stack is not empty
         while st:
+            # pop the top node off
             node = st.pop()
+
+            # if the node is in our dict 
             if node in cont:
+                # add to the stack to traverse
                 st.extend(cont[node])
+
+                # add to res to kill
                 res.extend(cont[node])
                 
         return res
